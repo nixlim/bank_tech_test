74fbeef598477bcf2690634f430def55cacffea8
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-18 15:21:12 +0100

    Refactor name of test doubles and injected classes

--- lib/bank.rb
+++ lib/bank.rb
@@ -2,12 +2,25 @@
 
 class Bank
 
-  def initialize (depositor = Deposit.new)
+  def initialize (
+      depositor = DepositMoney.new,
+      withdrawer = WithdrawMoney.new
+  )
     @depositor = depositor
   end
 
   def deposit(amount)
+    raise 'The deposit amount must be greater than 0' unless
+        check_for_incorrect_amount(amount)
     @depositor.add(amount)
   end
+
+  private
+
+  def check_for_incorrect_amount(amount)
+    amount > 0
+  end
+
+
 
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,10 +1,12 @@
 require 'bank'
 
 describe Bank do
-  let(:subject){described_class.new(depositor_double)}
-  let(:depositor_double) {spy :depositor_double}
+  let(:subject){described_class.new(deposit_money_instance_double, withdraw_money_instance_double)}
+  let(:deposit_money_instance_double) {spy :deposit_money_double}
+  let(:withdraw_money_instance_double) {spy :withdraw_money_double}
 
   describe '#deposit' do
+
     it 'should respond to #deposit method' do
       expect(subject).to respond_to(:deposit)
     end
@@ -15,7 +17,20 @@ describe Bank do
 
     it 'should call Depositor Class on deposit' do
       subject.deposit(10)
-      expect(depositor_double).to have_received(:add).with(10)
+      expect(deposit_money_instance_double).to have_received(:add).with(10)
+    end
+
+    it 'should throw an error if the #deposit amount is 0 or less' do
+      expect{subject.deposit(-10)}
+          .to raise_error("The deposit amount must be greater than 0")
+    end
+
+  end
+
+  describe '#withdraw' do
+
+    it 'should respond to withdraw method' do
+
     end
   end
 
