7eca710f0ecf9489ce792e52f57ebeae34dae8a2
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-18 18:11:02 +0100

    Add private #get_date method to DepositClass for date submission to ledger

--- lib/deposit_money.rb
+++ lib/deposit_money.rb
@@ -1,7 +1,14 @@
 class DepositMoney
 
   def add(amount, ledger)
-    ledger.credit(amount)
+    ledger.credit(amount, get_date)
+  end
+
+  private
+
+  def get_date
+    @date = Time.new.strftime("%d/%m/%Y")
+    p @date
   end
 
 end
--- spec/deposit_money_spec.rb
+++ spec/deposit_money_spec.rb
@@ -2,6 +2,7 @@ require 'deposit_money'
 
 describe DepositMoney do
   let(:ledger_instance_double) { spy :ledger_double, balance: 10 }
+  let(:date) { Time.new.strftime("%d/%m/%Y") }
 
   describe '#add' do
     it 'should take an amount as an argument' do
@@ -9,7 +10,7 @@ describe DepositMoney do
     end
     it 'should credit the Ledger Class' do
       subject.add(10, ledger_instance_double)
-      expect(ledger_instance_double).to have_received(:credit).with(10)
+      expect(ledger_instance_double).to have_received(:credit).with(10, date)
     end
   end
 
