648411a152037b36d467aedb2f3c28168fe1e1ab
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-18 17:41:13 +0100

    Add a test for #add method with one arg for DepositMoney Class

--- lib/deposit_money.rb
+++ lib/deposit_money.rb
@@ -1,3 +1,7 @@
 class DepositMoney
 
+  def add(amount)
+
+  end
+
 end
--- spec/deposit_money_spec.rb
+++ spec/deposit_money_spec.rb
@@ -1,5 +1,14 @@
 require 'deposit_money'
 
 describe DepositMoney do
+  describe '#add' do
+    it 'should respond to #add method' do
+      expect(subject).to respond_to(:add)
+    end
+    it 'should take an amount as an argument' do
+      expect(subject).to respond_to(:add).with(1).argument
+    end
+  end
+
 
 end
