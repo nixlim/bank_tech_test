5a90d76797a9374750c205d2e9e8c69d3cd7671b
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-18 16:42:47 +0100

    Add .rubocop.yml to the setup
    Amend the code in accordance with Rubocop review, make sure that /spec directory is excluded.

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,12 +1,10 @@
-
-
+# This class is a Command & Control Class for the application
 class Bank
-
-  def initialize (
+  def initialize(
       deposits_processor = DepositMoney.new,
       withdrawals_processor = WithdrawMoney.new,
       ledger = Ledger.new
-  )
+    )
     @deposits_processor = deposits_processor
     @withdrawals_processor = withdrawals_processor
     @ledger = ledger
@@ -23,17 +21,19 @@ class Bank
     @withdrawals_processor.add(amount)
   end
 
+  def statement
+    # code will go here
+  end
+
   private
 
   def check_for_incorrect_amount(amount)
     raise 'The amount must be greater than 0' unless
-        amount > 0
+        amount.positive?
   end
 
   def check_for_sufficient_balance(amount)
-    raise 'The balance is insufficient for withdrawal' unless @ledger.balance >= amount
+    raise 'The balance is insufficient for withdrawal' unless
+        @ledger.balance >= amount
   end
-
-
-
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -1,53 +1,55 @@
 require 'bank'
 
 describe Bank do
-  let(:subject){ described_class.new(
+  let(:subject) {
+    described_class.new(
       deposit_money_instance_double,
       withdraw_money_instance_double,
       ledger_instance_double
-  ) }
+    )
+  }
   let(:deposit_money_instance_double) { spy :deposit_money_double }
   let(:withdraw_money_instance_double) { spy :withdraw_money_double }
   let(:ledger_instance_double) { spy :ledger_double, balance: 10 }
 
   describe '#deposit' do
-
     it 'should take an amount to be deposited' do
       expect(subject).to respond_to(:deposit).with(1).argument
     end
-
     it 'should call DepositMoney Class on deposit' do
       subject.deposit(10)
-      expect(deposit_money_instance_double).to have_received(:add).with(10)
+      expect(deposit_money_instance_double)
+        .to have_received(:add).with(10)
     end
-
     it 'should throw an error if the #deposit amount is 0 or less' do
-      expect{subject.deposit(-10)}
-          .to raise_error("The amount must be greater than 0")
+      expect { subject.deposit(-10) }
+        .to raise_error('The amount must be greater than 0')
     end
-
   end
 
   describe '#withdraw' do
-
     it 'should take an amount to be withdrawn' do
       expect(subject).to respond_to(:withdraw).with(1).argument
     end
-
     it 'should call WithdrawMoney Class on withdrawal' do
       subject.withdraw(10)
-      expect(withdraw_money_instance_double).to have_received(:add).with(10)
+      expect(withdraw_money_instance_double)
+        .to have_received(:add).with(10)
     end
-
     it 'should raise an error if the #withdraw amount is less than 0' do
-      expect{ subject.withdraw(-10) }.to raise_error("The amount must be greater than 0")
+      expect { subject.withdraw(-10) }
+        .to raise_error('The amount must be greater than 0')
     end
-
-    it 'should check that the withdrawal does not exceed the available balance' do
+    it 'should check that the withdrawal does not exceed the available
+ balance' do
       subject.withdraw(5)
       expect(ledger_instance_double).to have_received(:balance)
     end
   end
 
-  
+  describe '#statement' do
+    it 'should respond to the #statement method' do
+      expect(subject).to respond_to(:statement)
+    end
+  end
 end
