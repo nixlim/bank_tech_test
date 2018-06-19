a090147362135eb6e18fbb2220763630719562f0
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-19 15:28:12 +0100

    Amend StatementPrinter Class to display decimals

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -2,13 +2,17 @@ class StatementPrinter
   def print_statement(ledger)
     puts header
     ledger.transactions_record.reverse.each { |record|
-      puts "#{record[:entry_date]} || #{record[:credit]} || #{record[:debit]} || #{record[:balance]}" }
+      puts "#{record[:entry_date]} || #{formatter(record[:credit])} || #{formatter(record[:debit])} || #{formatter(record[:balance])}" }
   end
 
   private
 
   def header
     'date || credit || debit || balance'
+  end
+
+  def formatter(value)
+    value === '' ? value : '%.2f' %value
   end
 
 
