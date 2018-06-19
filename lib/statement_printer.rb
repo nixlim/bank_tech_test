3d2ff9b75c9710f9417e6e9a286ba4604493193e
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-19 13:26:36 +0100

    Amend #print_statement of StatementPrinter Class
    Add a customisable private #header method to print header of the statement. Add iteration over ledger record to the #print_statement method. Add tests for puts output to confirm that the statement comes out correctly.

--- lib/statement_printer.rb
+++ lib/statement_printer.rb
@@ -1,3 +1,15 @@
 class StatementPrinter
+  def print_statement(ledger)
+    puts header
+    ledger.transactions_record.each { |record|
+      puts "#{record[:entry_date]} || #{record[:credit]} || #{record[:debit]} || #{record[:balance]}" }
+  end
+
+  private
+
+  def header
+    'date || credit || debit || balance'
+  end
+
 
 end
--- spec/statement_printer_spec.rb
+++ spec/statement_printer_spec.rb
@@ -1,5 +1,23 @@
 require 'statement_printer'
 
 describe StatementPrinter do
+  let(:ledger) {double :ledger_double, transactions_record: [
+    {:entry_date=>"19/06/2018", :entry_time=>"12:51", :credit=>10, :debit=>"", :balance=>10},
+    {:entry_date=>"19/06/2018", :entry_time=>"12:53", :credit=>"", :debit=>10, :balance=>0}
+  ]}
+
+  describe '#print_statement' do
+    it 'should respond to #print_statement with 1 argument' do
+      expect(subject).to respond_to(:print_statement)
+        .with(1).argument
+    end
+    it 'should print the statement to the screen with a heading' do
+      expect(STDOUT).to receive(:puts).with('date || credit || debit || balance')
+      expect(STDOUT).to receive(:puts).with('19/06/2018 || 10 ||  || 10')
+      expect(STDOUT).to receive(:puts).with('19/06/2018 ||  || 10 || 0')
+      subject.print_statement(ledger)
+    end
+
+  end
 
 end
