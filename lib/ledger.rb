812aed16a4615e4e3c9fd1dcdd317f559f84518d
Igor Ryabchuk <riabtchuk@gmail.com>
2018-06-19 12:39:48 +0100

    Amend Ledger Class variables and methods
    Add @transaction_record as an attr_reader. Add functionality to #debit and #credit methods to push the transaction to the @transaction_record. Add private #calculate_balance method to allow recording of the balance change on each transaction.

--- lib/ledger.rb
+++ lib/ledger.rb
@@ -1,12 +1,32 @@
 class Ledger
-  attr_reader :balance
+  attr_reader :balance, :transactions_record
+
+  def initialize
+    @transactions_record = []
+    @balance = 0
+  end
 
   def debit(amount, entry_date)
-
+    @transactions_record.push({
+      entry_date: entry_date.split(' ')[0],
+      entry_time: entry_date.split(' ')[1],
+      credit: '',
+      debit: amount,
+      balance: calculate_balance(-amount)})
   end
 
   def credit(amount, entry_date)
+    @transactions_record.push({
+      entry_date: entry_date.split(' ')[0],
+      entry_time: entry_date.split(' ')[1],
+      credit: amount,
+      debit: '',
+      balance: calculate_balance(amount)})
+  end
 
+  private
+  def calculate_balance(amount)
+    @balance += amount
   end
 
 end
--- spec/ledger_spec.rb
+++ spec/ledger_spec.rb
@@ -1,9 +1,27 @@
 require 'ledger'
 
 describe Ledger do
+  describe '@transactions_record' do
+    it 'should have an empty ledger array on initialisation' do
+      expect(subject.instance_variable_get(:@transactions_record))
+        .to be_an_instance_of(Array)
+      expect(subject.instance_variable_get(:@transactions_record))
+        .to be_empty
+    end
+  end
   describe '#debit' do
     it 'should respond to the #debit method' do
       expect(subject).to respond_to(:debit).with(2).arguments
+    end
+    it 'should push amount and date of entry into @transactions record array' do
+      subject.debit(10, Time.new.strftime("%d/%m/%Y %H:%M"))
+      expect(subject.instance_variable_get(:@transactions_record))
+        .to include({
+          entry_date: Time.new.strftime("%d/%m/%Y"),
+          entry_time: Time.new.strftime("%H:%M"),
+          credit: '',
+          debit: 10,
+          balance: -10})
     end
   end
 
@@ -11,12 +29,22 @@ describe Ledger do
     it 'should respond to the #credit method' do
       expect(subject).to respond_to(:credit).with(2).arguments
     end
+    it 'should push amount and date of entry into @transactions record array' do
+      subject.credit(10, Time.new.strftime("%d/%m/%Y %H:%M"))
+      expect(subject.instance_variable_get(:@transactions_record))
+        .to include({
+          entry_date: Time.new.strftime("%d/%m/%Y"),
+          entry_time: Time.new.strftime("%H:%M"),
+          credit: 10,
+          debit: '',
+          balance: 10})
+    end
   end
 
   describe '#balance method/attribute' do
     it 'should respond to #balance and have @balance var' do
       expect(subject).to respond_to(:balance)
-      expect(subject.balance).to eq(nil)
+      expect(subject.balance).to eq(0)
     end
   end
 
